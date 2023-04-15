e8c56b567bb6daba778152e45a64a3a45eb4ab7d
ankitahlwat <ankitahlwat54@gmail.com>
2023-04-15 12:56:18 -0700

    Add ArrayLists

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -1,9 +1,15 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Dijkstra {
     public String[] stringArray;
+    private ArrayList<DijkstraNode> unVisited;
+    private ArrayList<DijkstraNode> visited;
+    private HashMap<DijkstraNode, Integer> shortestLength;
+
 
     public String readStrings(final String filePath) {
         try {
@@ -18,6 +24,5 @@ public class Dijkstra {
     private void makeAdjacencyList(final String stringFromFile) {
         stringArray = stringFromFile.split("\n");
     }
-
 
 }
