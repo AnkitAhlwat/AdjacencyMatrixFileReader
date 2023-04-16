17d7e21c0cfe8a83390adba2805ca1ade64139fd
ankitahlwat <ankitahlwat54@gmail.com>
2023-04-15 18:28:26 -0700

    Complete Dijkstra algorithm

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -1,15 +1,13 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 
 public class Dijkstra {
     public String[] stringArray;
     private ArrayList<DijkstraNode> unVisited;
-    private ArrayList<DijkstraNode> visited;
-    private HashMap<DijkstraNode, Integer> shortestLength;
 
+    private HashMap<Integer, DijkstraNode> dijkstraNodeHashMap;
 
     public String readStrings(final String filePath) {
         try {
@@ -21,8 +19,52 @@ public class Dijkstra {
             return "Could Not Find File, Please Try Again";
         }
     }
+
     private void makeAdjacencyList(final String stringFromFile) {
         stringArray = stringFromFile.split("\n");
     }
 
+    public void findShortestPathsBFS(int startingNode, int endingNode) {
+        unVisited = new ArrayList<>();
+        dijkstraNodeHashMap = new HashMap<>();
+        makeNodeList();
+        dijkstraNodeHashMap.get(startingNode).setLengthFromStart(0);
+        Deque<DijkstraNode> dijkstraNodeDeque = new LinkedList<>();
+        dijkstraNodeDeque.add(dijkstraNodeHashMap.get(startingNode));
+        while (!dijkstraNodeDeque.isEmpty()) {
+            DijkstraNode currentNode = dijkstraNodeDeque.peek();
+            int currentNodePosition = currentNode.getInt();
+            for (int i = 0; i < stringArray.length; i++) {
+                if (i == currentNodePosition) {
+                    continue;
+                }
+                if (stringArray[currentNodePosition].charAt(i) == 1 && unVisited.contains(dijkstraNodeHashMap.get(i))) {
+                    DijkstraNode nextNode = dijkstraNodeHashMap.get(i);
+                    dijkstraNodeDeque.add(nextNode);
+                    int length = 1 + currentNode.getLengthFromStart();
+                    if (length <= nextNode.getLengthFromStart() || nextNode.getLengthFromStart() == null) {
+                        nextNode.setLengthFromStart(length);
+                        nextNode.getPreviousList().add(currentNode);
+                    }
+                }
+            }
+            unVisited.remove(dijkstraNodeDeque.poll());
+        } connectNodes(dijkstraNodeHashMap.get(endingNode));
+    }
+
+    private void makeNodeList() {
+        for (int i = 0; i < stringArray.length; i++) {
+            DijkstraNode currentNode = new DijkstraNode(i);
+            unVisited.add(currentNode);
+            dijkstraNodeHashMap.put(i, currentNode);
+        }
+    }
+    private void connectNodes(DijkstraNode endingNode){
+        if (endingNode.getPreviousList()!=null){
+            for (DijkstraNode node: endingNode.getPreviousList()) {
+                node.getNext().add(endingNode);
+                connectNodes(node);
+            }
+        }
+    }
 }
--- src/DijkstraNode.java
+++ src/DijkstraNode.java
@@ -3,16 +3,21 @@ import java.util.ArrayList;
 public class DijkstraNode {
     private  Object object;
     private ArrayList<DijkstraNode> next;
-    private ArrayList<DijkstraNode> previous;
+    private ArrayList<DijkstraNode> previousList;
+    private Integer lengthFromStart;
 
     public DijkstraNode(Object object) {
         this.object = object;
         next = new ArrayList<>();
-        previous = new ArrayList<>();
+        previousList = new ArrayList<>();
+        lengthFromStart = null;
     }
 
     public Object getObject() {
         return object;
+    }
+    public int getInt(){
+        return (int)object;
     }
 
     public void setObject(Object object) {
@@ -27,12 +32,20 @@ public class DijkstraNode {
         this.next = next;
     }
 
-    public ArrayList<DijkstraNode> getPrevious() {
-        return previous;
+    public ArrayList<DijkstraNode> getPreviousList() {
+        return previousList;
     }
 
-    public void setPrevious(ArrayList<DijkstraNode> previous) {
-        this.previous = previous;
+    public void setPreviousList(ArrayList<DijkstraNode> previousList) {
+        this.previousList = previousList;
+    }
+
+    public Integer getLengthFromStart() {
+        return lengthFromStart;
+    }
+
+    public void setLengthFromStart(Integer lengthFromStart) {
+        this.lengthFromStart = lengthFromStart;
     }
 
     @Override
