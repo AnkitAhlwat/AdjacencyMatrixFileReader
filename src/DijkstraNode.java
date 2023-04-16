07f5e492f1300738c7fa4918f80a970c118d194e
ankitahlwat <ankitahlwat54@gmail.com>
2023-04-15 21:01:12 -0700

    Polish file with checkstyle

--- src/DijkstraNode.java
+++ src/DijkstraNode.java
@@ -1,55 +1,117 @@
 import java.util.ArrayList;
 
+/**
+ * A node to be used on an unweighted graph using the Dijkstra Method.
+ *
+ * @author Ankit Ahlwat
+ * @version 2023
+ */
 public class DijkstraNode {
     private  Object object;
     private ArrayList<DijkstraNode> nextList;
     private ArrayList<DijkstraNode> previousList;
     private Integer lengthFromStart;
 
-    public DijkstraNode(Object object) {
+    /**
+     * Instantiates a new Dijkstra node.
+     *
+     * @param object the object
+     */
+    public DijkstraNode(final Object object) {
         this.object = object;
         nextList = new ArrayList<>();
         previousList = new ArrayList<>();
         lengthFromStart = null;
     }
 
+    /**
+     * Gets object.
+     *
+     * @return the object
+     */
     public Object getObject() {
         return object;
     }
-    public int getInt(){
-        return (int)object;
+
+    /**
+     * Get int int.
+     *
+     * @return the int
+     */
+    public int getInt() {
+        return (int) object;
     }
 
-    public void setObject(Object object) {
+    /**
+     * Sets object.
+     *
+     * @param object the object
+     */
+    public void setObject(final Object object) {
         this.object = object;
     }
 
+    /**
+     * Gets next list.
+     *
+     * @return the next list
+     */
     public ArrayList<DijkstraNode> getNextList() {
         return nextList;
     }
 
-    public void setNextList(ArrayList<DijkstraNode> nextList) {
+    /**
+     * Sets next list.
+     *
+     * @param nextList the next list
+     */
+    public void setNextList(final ArrayList<DijkstraNode> nextList) {
         this.nextList = nextList;
     }
 
+    /**
+     * Gets previous list.
+     *
+     * @return the previous list
+     */
     public ArrayList<DijkstraNode> getPreviousList() {
         return previousList;
     }
 
-    public void setPreviousList(ArrayList<DijkstraNode> previousList) {
+    /**
+     * Sets previous list.
+     *
+     * @param previousList the previous list
+     */
+    public void setPreviousList(final ArrayList<DijkstraNode> previousList) {
         this.previousList = previousList;
     }
 
+    /**
+     * Gets length from start.
+     *
+     * @return the length from start
+     */
     public Integer getLengthFromStart() {
         return lengthFromStart;
     }
 
-    public void setLengthFromStart(Integer lengthFromStart) {
+    /**
+     * Sets length from start.
+     *
+     * @param lengthFromStart the length from start
+     */
+    public void setLengthFromStart(final Integer lengthFromStart) {
         this.lengthFromStart = lengthFromStart;
     }
 
+    /**
+     * Return the object.
+     *
+     * @return the object
+     */
     @Override
     public String toString() {
-        return ""+object;
+        return "" + object;
     }
 }
